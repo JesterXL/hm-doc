f6b56d75974dabaf2a4a219614104fd1edb9bccd
Jesse Warden <jesterxl@jessewarden.com>
2018-09-30 19:00:56 -0400

    stupid path

--- bin/hm-doc.js
+++ bin/hm-doc.js
@@ -4,7 +4,7 @@ const { isString } = require('lodash/fp')
 const program = require('commander')
 
 program
-.version(require('./package.json').version)
+.version(require('../package.json').version)
 .option('-f, --files [demFiles]', 'File glob, list of files you want to load and parse comments out of [demFiles]. Example: ./*.js', './*.js')
 .parse(process.argv)
 
